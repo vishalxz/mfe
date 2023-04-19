aff61faf8d6b2fcdf11a232d7f3ba0157498b0a3
Vishal Ranjan <vishal.ranjan@Vishals-MacBook-Pro.local>
2023-04-19 23:52:30 +0530

    update

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,8 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-  },
+    publicPath: '/marketing/latest/',
+  }, 
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
